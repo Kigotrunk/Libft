eed77b11d208fdf778dc45841548edfaca5354ff
Kigotrunk <117681271+Kigotrunk@users.noreply.github.com>
2022-11-07 20:43:49 +0100

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,9 +1,9 @@
-void *memset (void *dest, int d, size_t len)
+void *memset (void *dest, int d, size_t n)
 {
   unsigned char *ptr;
 
   *ptr = dest;
   while (len-- > 0)
-    *ptr++ = val;
+    *ptr++ = n;
   return dest;
 }
