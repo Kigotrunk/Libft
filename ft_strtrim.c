6f32606fa02a294de0209bc02db69793b2b9ed26
Kigotrunk <117681271+Kigotrunk@users.noreply.github.com>
2022-11-14 10:35:16 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,44 +1,44 @@
-int ft_set(char const c, char const *s )
-{
-    int x;
+#include "libft.h"
 
-    x = 0;
-    while (s[x])
-    {
-        if (c == s[x])
-            return (0);
-        x++;
-    }
-    return (1);
+static int	ft_is_in_set(char c, char const *set)
+{
+	size_t	src;
+
+	src = 0;
+	while (set[src])
+	{
+		if (c == set[src])
+			return (1);
+		src++;
+	}
+	return (0);
 }
 
-char    *ft_strtrim(char const *s1, char const *set)
+char		*ft_strtrim(char const *s1, char const *set)
 {
-    char const *ptr;
-    int x;
-    int y;
-    int z;
+	int		src;
+	int		start;
+	int		end;
+	char	*ptr;
 
-    x = 0;
-    y = 0;
-    z = 0;
-
-    while(s1[x])
-    {
-        if (ft_set(s1[x], set))
-            y++;
-        x++;
-    }
-    ptr = (char const *)malloc(y * (char const))
-    x = 0;
-    while (s1[x])
-    {
-        if (ft_set(s1[x], set))
-        {
-            ptr[z] = s1[x];
-            z++;
-        }
-        x++;
-    }
-
+	if (s1 == NULL)
+		return (NULL);
+	if (set == NULL)
+		return ((char *)s1);
+	src = 0;
+	while (ft_is_in_set(s1[src], set) == 1)
+		src++;
+	start = src;
+	src = (ft_strlen(s1)) ? ft_strlen(s1) - 1 : 0;
+	while (ft_is_in_set(s1[src], set) == 1)
+		src--;
+	end = src;
+	if (start > end)
+		return ((char *)ft_calloc(1, sizeof(char)));
+	if (!(ptr = (char *)ft_calloc(end - start + 2, sizeof(char))))
+		return (NULL);
+	src = 0;
+	while (start <= end)
+		ptr[src++] = s1[start++];
+	return (ptr);
 }
